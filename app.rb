2200bb12207e55463fcbb9255db767737a4f3508
Jenni Choi <jenni0729@gmail.com>
2015-02-12 09:37:46 -0800

    homework

--- app.rb
+++ app.rb
@@ -25,11 +25,48 @@ get '/' do
 end
 
 # GET /users/new - display a form for making a new user
-#
+get '/users/new' do
+  erb :new
+end
 # POST /users - create a user based on params from form
-#
+post '/users' do
+  users.push first:params[:first], last:params[:last], id: id
+  id = id + 1
+  redirect '/'
+end
 # GET /users/:id - show a user's info by their id, this should display the info in a form
-#
+get '/users/:id' do
+  users.each do |user| 
+    if user[:id] == params[:id].to_i
+      @user = user
+    end
+  end
+    
+    erb :user
+  
+  
+end
 # PUT /users/:id - update a user's info based on the form from GET /users/:id
-#
+put '/users/:id' do
+  user = users[params[:id].to_i - 1]
+  user[:first] = params[:first]
+  user[:last] = params[:last]
+  
+  redirect '/'
+end
 # DELETE /users/:id - delete a user by their id
+delete '/users/:id' do
+
+  user =  users.find{|user| user[:id] == params[:id].to_i} 
+
+  users.delete(user) 
+  redirect '/' 
+ 
+end
+
+
+
+
+
+
+
